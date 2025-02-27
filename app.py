0e4eab31ccab74a967bfc8382b822853244d7a39
Jai Gulati <gulatijai@gmail.com>
2025-02-27 16:46:17 +1100

    data transformation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from src.DSProject.logger import logging
 from src.DSProject.exception import CustomException
 from src.DSProject.components.data_ingestion import DataIngestion
 from src.DSProject.components.data_ingestion import DataIngestionConfig
+from src.DSProject.components.data_transformation import DataTransfomationConfig, DataTransformation
+
 import sys
 
 if __name__== "__main__":
@@ -9,7 +11,11 @@ if __name__== "__main__":
 
     try:
         data_ingestion= DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path= data_ingestion.initiate_data_ingestion()
+        
+        #Data_Transformation_config= DataTransfomationConfig()
+        data_transformation= DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
        
     except Exception as e:
         logging.info("Custom exception")
--- src/DSProject/utils.py
+++ src/DSProject/utils.py
@@ -5,6 +5,9 @@ from src.DSProject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
+from sqlalchemy import create_engine
+import pickle
+import numpy as np
 
 load_dotenv()
 
@@ -23,11 +26,24 @@ def read_sql_data():
             db= db
         )
         logging.info("Established connection", mydb)
-        df= pd.read_sql_query('Select * from Students', mydb)
+        df= pd.read_sql_query('Select * from students', mydb)
         print(df.head())
         return df
 
 
     except Exception as ex:
         raise CustomException(ex,sys)
+    
+
+# saving pickle file
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open (file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException (e, sys)
     
